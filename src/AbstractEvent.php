52edf20660bac5bb936d2f16bfa353396d90dd2d
Alex Ermashev <alexermashev@medrecord.io>
2020-08-15 18:20:47 +0600

    change the copyright email

--- src/AbstractEvent.php
+++ src/AbstractEvent.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Tiny package.
  *
- * (c) Alex Ermashev <alexermashevn@gmail.com>
+ * (c) Alex Ermashev <alexermashev@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
